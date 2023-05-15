5c44dbf71ba7a77a82c3cf4e091f7e73cd153a04
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2023-05-15 22:53:02 +0500

    create send message to bulk users with next.js API, and vonage

---
+++ pages/api/send-bulk-message.js
@@ -0,0 +1,35 @@
+const { Vonage } = require("@vonage/server-sdk");
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+    console.log(data);
+    const vonage = new Vonage({
+      apiKey: "383558b9",
+      apiSecret: "8ztUraCHkCqEuN0P",
+    });
+
+    const smsPromises = data.map((recipient) => {
+      return vonage.sms.send({
+        to: recipient,
+        from: "Vonage APIs",
+        text: "Hello World!",
+      });
+    });
+
+    try {
+      const results = await Promise.all(smsPromises);
+      results.forEach((result, index) => {
+        console.log(result);
+      });
+
+      res.status(200).json({ message: "Messages sent successfully" });
+    } catch (error) {
+      console.error("Error processing the request:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  } else {
+    res.status(405).json({ message: "Method Not Allowed" });
+  }
+}
+
